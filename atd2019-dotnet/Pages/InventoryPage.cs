38c7a00d27bcbfd4250287a4c2d1e71fabe81bcf
andrewkrug <andrew.krug@saucelabs.com>
2019-11-07 13:48:41 +0100

    removing working code

--- atd2019-dotnet/Pages/InventoryPage.cs
+++ atd2019-dotnet/Pages/InventoryPage.cs
@@ -9,40 +9,7 @@ namespace workshop.Pages
     class InventoryPage
     {
         private IWebDriver driver;
-        private static String url = "https://www.saucedemo.com/inventory.html";
+        private static String url = "";
 
-        public static InventoryPage Visit(IWebDriver driver)
-        {
-            driver.Navigate().GoToUrl(url);
-            return new InventoryPage(driver);
-        }
-
-        public InventoryPage(IWebDriver driver)
-        {
-            this.driver = driver;
-        }
-
-        public Boolean onPage()
-        {
-            return driver.Url.Equals(url);
-        }
-
-        public IWebElement getInventoryItem(String product)
-        {
-            IList<IWebElement> items = driver.FindElements(By.ClassName("inventory_item_label"));
-            foreach (IWebElement item in items)
-            {
-                if (item.Text.Contains(product))
-                {
-                    return item.FindElement(By.TagName("a"));
-                }
-            }
-            return null;
-        }
-
-        public void selectProduct(Product product)
-        {
-            getInventoryItem(product.GetName()).Click();
-        }
     }
 }
---
+++ atd2019-dotnet/Tests/Example.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+using workshop.Base;
+
+namespace atd2019dotnet.Tests
+{
+    public class Example : Base
+    {
+        [Test]
+        public void ExampleTest()
+        {
+            Assert.That(true);
+        }
+        
+    }
+}
